4eab843fdc1cc3419bf12afe09b87a535e6b0582
Thomas Weibel <thom@ikhono.net>
2019-05-24 22:56:58 +0200

    Fix test cases

--- test/flightSurety.js
+++ test/flightSurety.js
@@ -1,4 +1,3 @@
-
 var Test = require('../config/testConfig.js');
 var BigNumber = require('bignumber.js');
 
@@ -15,15 +14,12 @@ contract('Flight Surety Tests', async (accounts) => {
   /****************************************************************************************/
 
   it(`(multiparty) has correct initial isOperational() value`, async function () {
-
     // Get operating status
     let status = await config.flightSuretyData.isOperational.call();
     assert.equal(status, true, "Incorrect initial operating status value");
-
   });
 
   it(`(multiparty) can block access to setOperatingStatus() for non-Contract Owner account`, async function () {
-
       // Ensure that access is denied for non-Contract Owner account
       let accessDenied = false;
       try 
@@ -34,11 +30,9 @@ contract('Flight Surety Tests', async (accounts) => {
           accessDenied = true;
       }
       assert.equal(accessDenied, true, "Access not restricted to Contract Owner");
-            
   });
 
   it(`(multiparty) can allow access to setOperatingStatus() for Contract Owner account`, async function () {
-
       // Ensure that access is allowed for Contract Owner account
       let accessDenied = false;
       try 
@@ -49,26 +43,26 @@ contract('Flight Surety Tests', async (accounts) => {
           accessDenied = true;
       }
       assert.equal(accessDenied, false, "Access not restricted to Contract Owner");
-      
+
+      // Set it back for other tests to work
+      await config.flightSuretyData.setOperatingStatus(true);
   });
 
   it(`(multiparty) can block access to functions using requireIsOperational when operating status is false`, async function () {
-
       await config.flightSuretyData.setOperatingStatus(false);
 
       let reverted = false;
       try 
       {
-          await config.flightSurety.setTestingMode(true);
+          await config.flightSurety.isOperational(true);
       }
       catch(e) {
           reverted = true;
       }
-      assert.equal(reverted, true, "Access not blocked for requireIsOperational");      
+      assert.equal(reverted, true, "Access not blocked for requireIsOperational");
 
       // Set it back for other tests to work
-      await config.flightSuretyData.setOperatingStatus(true);
-
+      //await config.flightSuretyData.setOperatingStatus(true);
   });
 
   it('(airline) cannot register an Airline using registerAirline() if it is not funded', async () => {
