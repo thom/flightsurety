ba1a00a41d7986fd74ac6a3b084faacb2df21564
Thomas Weibel <thom@ikhono.net>
2019-05-26 14:59:32 +0200

    Add enough addresses for oracles

--- 1_start_ganache-cli.sh
+++ 1_start_ganache-cli.sh
@@ -1,1 +1,1 @@
-ganache-cli  --gasLimit 300000000 --gasPrice 20000000000 -m "forget chief exist liberty video cash twelve nest grief umbrella panel trumpet"
+ganache-cli  --gasLimit 300000000 --gasPrice 20000000000 -a 50 -m "forget chief exist liberty video cash twelve nest grief umbrella panel trumpet"
