6d3bbd15fc64fbe1276ac4cf65cb3d05313bc1a1
Thomas Weibel <thom@ikhono.net>
2019-05-26 22:15:40 +0200

    Reformat code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,20 +3,19 @@ const FlightSuretyData = artifacts.require("FlightSuretyData");
 const fs = require('fs');
 
 module.exports = function(deployer, network, accounts) {
-    let firstAirline = accounts[1];
-    deployer.deploy(FlightSuretyData)
-    .then(() => {
-        return deployer.deploy(FlightSuretyApp, FlightSuretyData.address)
-                .then(() => {
-                    let config = {
-                        localhost: {
-                            url: 'http://localhost:8545',
-                            dataAddress: FlightSuretyData.address,
-                            appAddress: FlightSuretyApp.address
-                        }
-                    }
-                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                    fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                });
+  let firstAirline = accounts[1];
+  deployer.deploy(FlightSuretyData).then(() => {
+    // Airline Contract Initialization: First airline is registered when contract is deployed
+    return deployer.deploy(FlightSuretyApp, FlightSuretyData.address, firstAirline).then(() => {
+      let config = {
+        localhost: {
+          url: 'http://localhost:8545',
+          dataAddress: FlightSuretyData.address,
+          appAddress: FlightSuretyApp.address
+        }
+      }
+      fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+      fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
     });
+  });
 }
