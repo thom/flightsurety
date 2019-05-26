340b5ff4c84341e061d91bde076358c126a757be
Thomas Weibel <thom@ikhono.net>
2019-05-26 15:00:05 +0200

    Reformat code

--- contracts/FlightSuretyData.sol
+++ contracts/FlightSuretyData.sol
@@ -3,219 +3,177 @@ pragma solidity ^0.5.0;
 import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract FlightSuretyData {
-    using SafeMath for uint256;
+  using SafeMath for uint256;
 
-    /********************************************************************************************/
-    /*                                       DATA VARIABLES                                     */
-    /********************************************************************************************/
+  /********************************************************************************************/
+  /*                                       DATA VARIABLES                                     */
+  /********************************************************************************************/
 
-    address private contractOwner;                                      // Account used to deploy contract
-    bool private operational = true;                                    // Blocks all state changes throughout the contract if false
+  address private contractOwner;                                      // Account used to deploy contract
+  bool private operational = true;                                    // Blocks all state changes throughout the contract if false
 
-    // Multi-party consensus
-    // Added to setOperational() to practice the concept
-    uint constant M = 1;
-    address[] multiCalls = new address[](0);
+  // Multi-party consensus
+  // Added to setOperational() to practice the concept
+  uint constant M = 1;
+  address[] multiCalls = new address[](0);
 
-    // Restrict data contract callers
-    mapping(address => uint256) private authorizedContracts;
+  // Restrict data contract callers
+  mapping(address => uint256) private authorizedContracts;
 
-    /********************************************************************************************/
-    /*                                       EVENT DEFINITIONS                                  */
-    /********************************************************************************************/
+  /********************************************************************************************/
+  /*                                       EVENT DEFINITIONS                                  */
+  /********************************************************************************************/
 
+  /**
+  * @dev Constructor
+  *      The deploying account becomes contractOwner
+  */
+  constructor() public {
+      contractOwner = msg.sender;
+  }
 
-    /**
-    * @dev Constructor
-    *      The deploying account becomes contractOwner
-    */
-    constructor
-                                (
-                                ) 
-                                public 
-    {
-        contractOwner = msg.sender;
-    }
+  /********************************************************************************************/
+  /*                                       FUNCTION MODIFIERS                                 */
+  /********************************************************************************************/
 
-    /********************************************************************************************/
-    /*                                       FUNCTION MODIFIERS                                 */
-    /********************************************************************************************/
+  // Modifiers help avoid duplication of code. They are typically used to validate something
+  // before a function is allowed to be executed.
 
-    // Modifiers help avoid duplication of code. They are typically used to validate something
-    // before a function is allowed to be executed.
+  /**
+  * @dev Modifier that requires the "operational" boolean variable to be "true"
+  *      This is used on all state changing functions to pause the contract in 
+  *      the event there is an issue that needs to be fixed
+  */
+  modifier requireIsOperational() {
+    require(isOperational(), "Contract is currently not operational");
+    _;  // All modifiers require an "_" which indicates where the function body will be added
+  }
 
-    /**
-    * @dev Modifier that requires the "operational" boolean variable to be "true"
-    *      This is used on all state changing functions to pause the contract in 
-    *      the event there is an issue that needs to be fixed
-    */
-    modifier requireIsOperational() {
-        require(isOperational(), "Contract is currently not operational");
-        _;  // All modifiers require an "_" which indicates where the function body will be added
-    }
+  /**
+  * @dev Modifier that requires the "ContractOwner" account to be the function caller
+  */
+  modifier requireContractOwner() {
+    require(msg.sender == contractOwner, "Caller is not contract owner");
+    _;
+  }
 
-    /**
-    * @dev Modifier that requires the "ContractOwner" account to be the function caller
-    */
-    modifier requireContractOwner() {
-        require(msg.sender == contractOwner, "Caller is not contract owner");
-        _;
-    }
+  /**
+  * @dev Modifier that requires function caller to be authorized
+  */
+  modifier requireIsCallerAuthorized() {
+    require(authorizedContracts[msg.sender] == 1, "Caller is not authorized");
+    _;
+  }
 
-    /**
-    * @dev Modifier that requires function caller to be authorized
-    */
-    modifier requireIsCallerAuthorized() {
-        require(authorizedContracts[msg.sender] == 1, "Caller is not authorized");
-        _;
-    }
+  /********************************************************************************************/
+  /*                                       UTILITY FUNCTIONS                                  */
+  /********************************************************************************************/
 
-    /********************************************************************************************/
-    /*                                       UTILITY FUNCTIONS                                  */
-    /********************************************************************************************/
+  /**
+  * @dev Get operating status of contract
+  *
+  * @return A bool that is the current operating status
+  */      
+  function isOperational() public view returns(bool) {
+    return operational;
+  }
 
-    /**
-    * @dev Get operating status of contract
-    *
-    * @return A bool that is the current operating status
-    */      
-    function isOperational() public view returns(bool) {
-      return operational;
-    }
+  /**
+  * @dev Sets contract operations on/off
+  *
+  * When operational mode is disabled, all write transactions except for this one will fail
+  */    
+  function setOperatingStatus(bool mode) external requireContractOwner {
+    require(mode != operational, "New mode must be different from existing mode");
 
-    /**
-    * @dev Sets contract operations on/off
-    *
-    * When operational mode is disabled, all write transactions except for this one will fail
-    */    
-    function setOperatingStatus(bool mode) external requireContractOwner {
-      require(mode != operational, "New mode must be different from existing mode");
+    bool isDuplicate = false;
 
-      bool isDuplicate = false;
-
-      for(uint i = 0; i < multiCalls.length; i++) {
-        if (multiCalls[i] == msg.sender) {
-          isDuplicate = true;
-          break;
-        }
-      }
-
-      require(!isDuplicate, "Caller has already called this function.");
-
-      multiCalls.push(msg.sender);
-
-      if (multiCalls.length >= M) {
-        operational = mode;
-        multiCalls = new address[](0);
+    for(uint i = 0; i < multiCalls.length; i++) {
+      if (multiCalls[i] == msg.sender) {
+        isDuplicate = true;
+        break;
       }
     }
 
-    /**
-    * @dev Adds address to authorized contracts
-    */
-    function authorizeCaller(address contractAddress) external requireContractOwner {
-      authorizedContracts[contractAddress] = 1;
+    require(!isDuplicate, "Caller has already called this function.");
+
+    multiCalls.push(msg.sender);
+
+    if (multiCalls.length >= M) {
+      operational = mode;
+      multiCalls = new address[](0);
     }
+  }
 
-    /**
-    * @dev Removes address from authorized contracts
-    */
-    function deauthorizeCaller(address contractAddress) external requireContractOwner {
-      delete authorizedContracts[contractAddress];
-    }
+  /**
+  * @dev Adds address to authorized contracts
+  */
+  function authorizeCaller(address contractAddress) external requireContractOwner {
+    authorizedContracts[contractAddress] = 1;
+  }
 
-    /********************************************************************************************/
-    /*                                     SMART CONTRACT FUNCTIONS                             */
-    /********************************************************************************************/
+  /**
+  * @dev Removes address from authorized contracts
+  */
+  function deauthorizeCaller(address contractAddress) external requireContractOwner {
+    delete authorizedContracts[contractAddress];
+  }
 
-   /**
-    * @dev Add an airline to the registration queue
-    *      Can only be called from FlightSuretyApp contract
+  /********************************************************************************************/
+  /*                                     SMART CONTRACT FUNCTIONS                             */
+  /********************************************************************************************/
+
+  /**
+  * @dev Add an airline to the registration queue
+  *      Can only be called from FlightSuretyApp contract
+  *
+  */   
+  function registerAirline() external requireIsOperational {
+
+  }
+
+
+  /**
+  * @dev Buy insurance for a flight
+  *
+  */   
+  function buy() external payable requireIsOperational {
+
+  }
+
+  /**
+    *  @dev Credits payouts to insurees
+  */
+  function creditInsurees() external requireIsOperational {
+  }
+  
+
+  /**
+    *  @dev Transfers eligible payout funds to insuree
     *
-    */   
-    function registerAirline
-                            (   
-                            )
-                            external
-                            requireIsOperational
-    {
-    }
+  */
+  function pay() external requireIsOperational {
 
+  }
 
-   /**
-    * @dev Buy insurance for a flight
-    *
-    */   
-    function buy
-                            (                             
-                            )
-                            external
-                            payable
-                            requireIsOperational
-    {
+  /**
+  * @dev Initial funding for the insurance. Unless there are too many delayed flights
+  *      resulting in insurance payouts, the contract should be self-sustaining
+  *
+  */   
+  function fund() public payable requireIsOperational {
 
-    }
+  }
 
-    /**
-     *  @dev Credits payouts to insurees
-    */
-    function creditInsurees
-                                (
-                                )
-                                external
-                                requireIsOperational
-    {
-    }
-    
+  function getFlightKey(address airline, string memory flight, uint256 timestamp) pure internal returns(bytes32) {
+    return keccak256(abi.encodePacked(airline, flight, timestamp));
+  }
 
-    /**
-     *  @dev Transfers eligible payout funds to insuree
-     *
-    */
-    function pay
-                            (
-                            )
-                            external
-                            requireIsOperational
-    {
-    }
-
-   /**
-    * @dev Initial funding for the insurance. Unless there are too many delayed flights
-    *      resulting in insurance payouts, the contract should be self-sustaining
-    *
-    */   
-    function fund
-                            (   
-                            )
-                            public
-                            payable
-                            requireIsOperational
-    {
-    }
-
-    function getFlightKey
-                        (
-                            address airline,
-                            string memory flight,
-                            uint256 timestamp
-                        )
-                        pure
-                        internal
-                        returns(bytes32) 
-    {
-        return keccak256(abi.encodePacked(airline, flight, timestamp));
-    }
-
-    /**
-    * @dev Fallback function for funding smart contract.
-    *
-    */
-    function() 
-                            external 
-                            payable
-    {
-        fund();
-    }
+  /**
+  * @dev Fallback function for funding smart contract.
+  *
+  */
+  function() external payable {
+    fund();
+  }
 }
-
--- test/oracles.js
+++ test/oracles.js
@@ -3,9 +3,9 @@ var Test = require('../config/testConfig.js');
 //var BigNumber = require('bignumber.js');
 
 contract('Oracles', async (accounts) => {
-
   const TEST_ORACLES_COUNT = 20;
   var config;
+
   before('setup contract', async () => {
     config = await Test.Config(accounts);
 
@@ -16,12 +16,10 @@ contract('Oracles', async (accounts) => {
     const STATUS_CODE_LATE_WEATHER = 30;
     const STATUS_CODE_LATE_TECHNICAL = 40;
     const STATUS_CODE_LATE_OTHER = 50;
-
   });
 
 
   it('can register oracles', async () => {
-    
     // ARRANGE
     let fee = await config.flightSuretyApp.REGISTRATION_FEE.call();
 
@@ -34,41 +32,31 @@ contract('Oracles', async (accounts) => {
   });
 
   it('can request flight status', async () => {
-    
     // ARRANGE
     let flight = 'ND1309'; // Course number
     let timestamp = Math.floor(Date.now() / 1000);
 
     // Submit a request for oracles to get status information for a flight
     await config.flightSuretyApp.fetchFlightStatus(config.firstAirline, flight, timestamp);
-    // ACT
 
+    // ACT
     // Since the Index assigned to each test account is opaque by design
     // loop through all the accounts and for each account, all its Indexes (indices?)
     // and submit a response. The contract will reject a submission if it was
     // not requested so while sub-optimal, it's a good test of that feature
     for(let a=1; a<TEST_ORACLES_COUNT; a++) {
-
       // Get oracle information
       let oracleIndexes = await config.flightSuretyApp.getMyIndexes.call({ from: accounts[a]});
       for(let idx=0;idx<3;idx++) {
-
         try {
           // Submit a response...it will only be accepted if there is an Index match
           await config.flightSuretyApp.submitOracleResponse(oracleIndexes[idx], config.firstAirline, flight, timestamp, STATUS_CODE_ON_TIME, { from: accounts[a] });
-
         }
         catch(e) {
           // Enable this when debugging
            console.log('\nError', idx, oracleIndexes[idx].toNumber(), flight, timestamp);
         }
-
       }
     }
-
-
   });
-
-
- 
 });
