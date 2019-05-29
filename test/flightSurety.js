2d85ec87725ba934f97e94ed049f27b05da15193
Thomas Weibel <thom@ikhono.net>
2019-05-29 21:23:31 +0200

    Test airline functionality

--- test/flightSurety.js
+++ test/flightSurety.js
@@ -3,6 +3,11 @@ var BigNumber = require('bignumber.js');
 
 contract('Flight Surety Tests', async (accounts) => {
   var config;
+  const AIRLINE_FUNDING_VALUE = web3.utils.toWei("10", "ether");
+  let airline2 = accounts[2];
+  let airline3 = accounts[3];
+  let airline4 = accounts[4];
+  let airline5 = accounts[5];
 
   before('setup contract', async () => {
     config = await Test.Config(accounts);
@@ -62,31 +67,110 @@ contract('Flight Surety Tests', async (accounts) => {
     await config.flightSuretyData.setOperatingStatus(true);
   });
 
-  it('(airline) cannot register an Airline using registerAirline() if it is not funded', async () => {
-    // ARRANGE
-    let newAirline = accounts[2];
+  // Airline Contract Initialization: First airline is registered when contract is deployed.
+  it('(airline) first airline is registered when contract is deployed', async () => {
+    let airlineName = await config.flightSuretyData.getAirlineName(config.firstAirlineAddress, { from: config.owner });
+    assert.equal(airlineName, config.firstAirlineName, "First airline is not registered when contract is deployed")
+  });
 
-    // ACT
+  // Airline Ante: Airline can be registered, but does not participate in contract until it submits funding of 10 ether
+  it('(airline) cannot register an airline using registerAirline() if it is not funded', async () => {
     try {
-      await config.flightSuretyApp.registerAirline("Lufthansa", newAirline, {from: config.firstAirlineAddress});
+      await config.flightSuretyApp.registerAirline("Cebu Pacific Air", airline2, {from: config.firstAirlineAddress});
     }
     catch(e) {
       //console.log(e);
     }
-    let result = await config.flightSuretyData.isAirline.call(newAirline); 
+    let result = await config.flightSuretyData.isAirline.call(airline2); 
 
-    // ASSERT
     assert.equal(result, false, "Airline should not be able to register another airline if it hasn't provided funding");
   });
 
-  it('(airline) TODO', async () => {
-    // ARRANGE
-    //TODO
+  // Airline Ante: Airline can be registered, but does not participate in contract until it submits funding of 10 ether
+  it('(airline) airline needs to be funded with 10 ether', async () => {
+    const AIRLINE_FUNDING_VALUE_LOWER = web3.utils.toWei("5", "ether");
 
-    // ACT
-    //TODO
+    let reverted = false;
+    try {
+      await config.flightSuretyApp.fundAirline({ from: config.firstAirlineAddress, value: AIRLINE_FUNDING_VALUE_LOWER, gasPrice: 0 })
+    }
+    catch(e) {
+      reverted = true;
+    }
 
-    // ASSERT
-    //TODO
+    assert.equal(reverted, true, "Airline cannot be funded with less than 10 ether");
+  });
+
+  // Airline Ante: Airline can be registered, but does not participate in contract until it submits funding of 10 ether
+  it('(airline) airline does participate in contract when it is funded', async () => {
+    await config.flightSuretyApp.fundAirline({ from: config.firstAirlineAddress, value: AIRLINE_FUNDING_VALUE, gasPrice: 0 })
+
+    try {
+      await config.flightSuretyApp.registerAirline("Cebu Pacific Air", airline2, {from: config.firstAirlineAddress});
+    }
+    catch(e) {
+      //console.log(e);
+    }
+    let result = await config.flightSuretyData.isAirline.call(airline2); 
+
+    assert.equal(result, true, "Airline should be able to register another airline if it has been funded");
+  });
+
+  it('(airline) cannot register an airline more than once', async () => {
+    let reverted = false;
+    try {
+       await config.flightSuretyApp.registerAirline("Cebu Pacific Air", airline2, {from: config.firstAirlineAddress});
+    }
+    catch(e) {
+      reverted = true;
+    }
+
+    assert.equal(reverted, true, "Airline cannot be registered twice");
+  });
+
+  // Multiparty Consensus: Only existing airline may register a new airline until there are at least four airlines registered
+  it('(airline) can register up to 4 airlines', async () => {
+    let result = undefined;
+
+    try {
+        await config.flightSuretyApp.registerAirline("Philippine Airlines", airline3, {from: config.firstAirlineAddress});
+    }
+    catch(e) {
+    }
+    result = await config.flightSuretyData.isAirline.call(airline3);
+    assert.equal(result, true, "Registering the third airline should be possible");
+
+    try {
+        await config.flightSuretyApp.registerAirline("Singapore Airlines", airline4, {from: config.firstAirlineAddress});
+    }
+    catch(e) {
+    }
+    result = await config.flightSuretyData.isAirline.call(airline4);
+    assert.equal(result, true, "Registering the fourth airline should be possible");
+  });
+
+  // Multiparty Consensus: Registration of fifth and subsequent airlines requires multi-party consensus of 50% of registered airlines
+  it('(airline) single airline cannot register 5th airline on its own', async () => {
+    let result = undefined;
+
+    try {
+        await config.flightSuretyApp.registerAirline("Cathay Pacific", airline5, {from: config.firstAirlineAddress});
+    }
+    catch(e) {
+    }
+    result = await config.flightSuretyData.isAirline.call(airline5);
+    assert.equal(result, false, "Registering the fifth airline should not be possible");
+  });
+
+  it('(airline) 5th airline requires multi-party consensus of 50% of registered airlines', async () => {
+    await config.flightSuretyApp.fundAirline({ from: airline2, value: AIRLINE_FUNDING_VALUE, gasPrice: 0 })
+
+    try {
+        await config.flightSuretyApp.registerAirline("Cathay Pacific", airline5, {from: airline2});
+    }
+    catch(e) {
+    }
+    result = await config.flightSuretyData.isAirline.call(airline5);
+    assert.equal(result, true, "Registering the fifth airline should be possible");
   });
 });
