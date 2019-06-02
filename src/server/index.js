9eccb6f084b78229ba8df1b2dcc64aa5f0a7c1e0
Thomas Weibel <thom@ikhono.net>
2019-06-02 22:05:30 +0200

    Reformat code

--- src/server/index.js
+++ src/server/index.js
@@ -2,14 +2,15 @@
 import http from 'http'
 import app from './server'
 
-const server = http.createServer(app)
-let currentApp = app
-server.listen(3000)
+const server = http.createServer(app);
+let currentApp = app;
+server.listen(3000);
+console.log("Server listening at http://localhost:3000");
 
 if (module.hot) {
- module.hot.accept('./server', () => {
-  server.removeListener('request', currentApp)
-  server.on('request', app)
-  currentApp = app
- })
+  module.hot.accept('./server', () => {
+    server.removeListener('request', currentApp);
+    server.on('request', app);
+    currentApp = app;
+  });
 }
